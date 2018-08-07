92fcc03d2e322e13d3848a6374dc1e551b8ee4fc
kishan <kishan.kushwaha11@gmail.com>
2018-08-07 13:06:52 +0530

    mode pending

---
+++ src/MeanMedianMode.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MeanMedianMode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        float sum = 0.0f;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            sum = sum + a[i];
+        }
+
+        float mean = sum / n;
+        System.out.printf("%.1f\n", mean);
+
+        Arrays.sort(a);
+        float median = (float) ((a[(n-1)/2] + a[((n-1)/2)+1])/2.0);
+        System.out.printf("%.1f\n", median);
+
+        int mode = a[0];
+        int count1 = 1, count2 = 1;
+        for(int i=1; i<n; i++){
+            if(a[i]<=a[i-1]) {
+                count1++;
+                if(count1>count2)
+                    mode = a[i];
+            }
+            else
+            {
+                count2 = count1;
+                count1 = 1;
+                continue;
+            }
+        }
+
+        System.out.println(mode);
+    }
+}
