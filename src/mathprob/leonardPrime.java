093003c67c6f56c1bfa62f00f13fd8888488c13e
kishan <kishan.kushwaha11@gmail.com>
2018-08-30 23:05:45 +0530

    Leonard prime (1 Testcase left)

--- src/mathprob/leonardPrime.java
+++ src/mathprob/leonardPrime.java
@@ -15,7 +15,7 @@ public class leonardPrime {
     static int primeCount(long n) {
 
         int count = 0;
-        long product = 1;
+        double product = 1;
         int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53};
         for(int i=0; i<prime.length; i++){
             product *= prime[i];
