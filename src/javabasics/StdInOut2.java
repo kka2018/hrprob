68f9aaa153be478b696f051a22726b8c07c3fddd
kka2018 <kishan.kushwaha11@gmail.com>
2018-09-09 17:43:42 +0530

    next elaborated

--- src/javabasics/StdInOut2.java
+++ src/javabasics/StdInOut2.java
@@ -7,7 +7,14 @@ public class StdInOut2 {
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
         double y=sc.nextDouble();
-        sc.nextLine(); // To avoid the empty line left after nexDouble
+/*
+        If you use the nextLine() method immediately following the nextInt() method,
+        recall that nextInt() reads integer tokens; because of this,
+        the last newline character for that line of integer input is
+        still queued in the input buffer and the next nextLine() will
+        be reading the remainder of the integer line (which is empty).
+*/
+        sc.nextLine(); // To avoid the empty line left after nextDouble
         String s=sc.nextLine();
 
 
